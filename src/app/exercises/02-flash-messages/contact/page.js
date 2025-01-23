323366cc03deb97970c017f0e4e3c98bb040f05c
Lenka Šmitalová <>
2025-01-23 21:37:35 +0100

    flash messages exercise solution

--- src/app/exercises/02-flash-messages/contact/page.js
+++ src/app/exercises/02-flash-messages/contact/page.js
@@ -1,15 +1,37 @@
 'use client';
 import React from 'react';
+import { useRouter } from 'next/navigation';
+import { ToastContext } from '../../../../components/ToastProvider';
 
 function ContactPage() {
+  const [name, setName] = React.useState('');
+  const { createToast } = React.useContext(ToastContext);
+  const router = useRouter();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    router.push('/exercises/02-flash-messages');
+
+    createToast(
+      `Thank you ${name}! Your message has been received! We'll get back to you shortly.`,
+      'success'
+    );
+  }
+
   return (
     <main>
-      <form>
-        <label htmlFor="name">Name:</label>
-        <input id="name" required={true} />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='name'>Name:</label>
+        <input
+          id='name'
+          required={true}
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
 
-        <label htmlFor="message">Message:</label>
-        <textarea id="message" />
+        <label htmlFor='message'>Message:</label>
+        <textarea id='message' />
 
         <button>Submit</button>
       </form>
--- src/app/exercises/02-flash-messages/layout.js
+++ src/app/exercises/02-flash-messages/layout.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import ToastProvider from '../../../components/ToastProvider';
 
 import './styles.css';
+import ToastShelf from '../../../components/ToastShelf/ToastShelf';
 
 function FlashMsgLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang='en'>
+      <body>
+        <ToastProvider>
+          {children}
+          <ToastShelf />
+        </ToastProvider>
+      </body>
     </html>
   );
 }
